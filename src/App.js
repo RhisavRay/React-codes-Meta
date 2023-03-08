585d67b060cf16e7f38606d620cf066d78552086
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-03-08 21:26:36 +0530

    learned to add images to react files, and installed react-player for video input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import './App.css'
+import rooftop from './assets/central-park.jpg'
 
 function App()
 {
-  return
+  return (
+    <div className='App'>
+      <h1>Task: Add three images with some styling</h1>
+      
+      <h3>Using import statement:</h3>
+      <img
+      height={200}
+      src={rooftop}
+      alt="A rooftop view of Central Park, New York"/>
+      
+      <h3>Using require statement:</h3>
+      <img
+      height={200}
+      src={require("./assets/central-park.jpg")}
+      alt="A rooftop view of Central Park, New York"/>
+
+      <h3>Using online url:</h3>
+      <img
+      height={200}
+      /*src={randomImageURL}*/
+      alt="A rooftop view of Central Park, New York"/>
+    </div>
+  )
 }
 
 export default App;
