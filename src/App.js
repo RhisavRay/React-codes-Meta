3b2263e85014a13ff214814675e885f7324f0452
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-03-08 21:43:39 +0530

    Used react-player to display video assets on a webpage

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import './App.css'
-import rooftop from './assets/central-park.jpg'
+import ReactPlayer from 'react-player';
 
 function App()
 {
   return (
     <div className='App'>
-      <h1>Task: Add three images with some styling</h1>
-      
-      <h3>Using import statement:</h3>
-      <img
-      height={200}
-      src={rooftop}
-      alt="A rooftop view of Central Park, New York"/>
-      
-      <h3>Using require statement:</h3>
-      <img
-      height={200}
-      src={require("./assets/central-park.jpg")}
-      alt="A rooftop view of Central Park, New York"/>
-
-      <h3>Using online url:</h3>
-      <img
-      height={200}
-      /*src={randomImageURL}*/
-      alt="A rooftop view of Central Park, New York"/>
+      <h1>React player example</h1>
+      <ReactPlayer
+      url={"https://www.youtube.com/watch?v=y1Pjp4rHavY"}
+      playing={false}
+      controls={true}
+      volume={0.5}/>
     </div>
   )
 }
